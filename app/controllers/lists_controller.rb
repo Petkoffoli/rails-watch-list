ae887f3f00e6fc709d7f3a8a4c50353b92c35467
Pedro Zoffoli <pedrozoffoli@gmail.com>
2023-05-25 19:20:44 -0300

    finished all controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def new
